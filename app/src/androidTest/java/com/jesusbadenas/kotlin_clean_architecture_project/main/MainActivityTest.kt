f4cfffdb5b20885c2cd8b12ce716cb417127bec4
Jesús Badenas <chusobadenas@gmail.com>
2025-01-21 18:12:39 +0100

    Fix unit tests

--- app/src/androidTest/java/com/jesusbadenas/kotlin_clean_architecture_project/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/jesusbadenas/kotlin_clean_architecture_project/main/MainActivityTest.kt
@@ -7,13 +7,17 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.jesusbadenas.kotlin_clean_architecture_project.data.di.dataModule
+import com.jesusbadenas.kotlin_clean_architecture_project.domain.di.domainModule
 import com.jesusbadenas.kotlin_clean_architecture_project.presentation.R
+import com.jesusbadenas.kotlin_clean_architecture_project.presentation.di.presentationModule
 import com.jesusbadenas.kotlin_clean_architecture_project.presentation.main.MainActivity
+import com.jesusbadenas.kotlin_clean_architecture_project.test.CustomKoinJUnit4Test
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MainActivityTest {
+class MainActivityTest: CustomKoinJUnit4Test(dataModule, domainModule, presentationModule) {
 
     @Test
     fun testMainFragmentIsOpenSuccess() {
--- app/src/androidTest/java/com/jesusbadenas/kotlin_clean_architecture_project/userlist/UserListFragmentTest.kt
+++ app/src/androidTest/java/com/jesusbadenas/kotlin_clean_architecture_project/userlist/UserListFragmentTest.kt
@@ -10,13 +10,17 @@ import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.jesusbadenas.kotlin_clean_architecture_project.data.di.dataModule
+import com.jesusbadenas.kotlin_clean_architecture_project.domain.di.domainModule
 import com.jesusbadenas.kotlin_clean_architecture_project.presentation.R
+import com.jesusbadenas.kotlin_clean_architecture_project.presentation.di.presentationModule
 import com.jesusbadenas.kotlin_clean_architecture_project.presentation.main.MainActivity
+import com.jesusbadenas.kotlin_clean_architecture_project.test.CustomKoinJUnit4Test
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class UserListFragmentTest {
+class UserListFragmentTest: CustomKoinJUnit4Test(dataModule, domainModule, presentationModule)  {
 
     @Test
     fun testNavigateToDetailFragmentSuccess() {
