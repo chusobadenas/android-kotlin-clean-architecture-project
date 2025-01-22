10ab1a4e9556b7a111f5c2f04d9feeab1bdacedb
Jesús Badenas <chusobadenas@gmail.com>
2025-01-22 11:28:06 +0100

    Add error listener

--- presentation/src/main/java/com/jesusbadenas/kotlin_clean_architecture_project/presentation/binding/BindingAdapter.kt
+++ presentation/src/main/java/com/jesusbadenas/kotlin_clean_architecture_project/presentation/binding/BindingAdapter.kt
@@ -9,12 +9,18 @@ import coil3.request.crossfade
 import coil3.request.error
 import coil3.request.placeholder
 import com.jesusbadenas.kotlin_clean_architecture_project.presentation.R
+import timber.log.Timber
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageUrl(imageUrl: String?) = this.load(imageUrl) {
     crossfade(true)
     error(R.color.error)
     placeholder(R.color.error)
+    listener(
+        onError = { _, result ->
+            Timber.e(result.throwable)
+        }
+    )
 }
 
 @BindingAdapter("isVisible")
